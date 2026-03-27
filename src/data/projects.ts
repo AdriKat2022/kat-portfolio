f6d435dc2060e85e1c49b9bd90171d715dec2f25
AdriKat <adrien.schroedel@telecom-sudparis.eu>
2026-03-27 02:43:39 +0100

    updated link

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -179,11 +179,11 @@ export const projects: Project[] = [
     actions: [
       {
         type: 'PLAY-IN-BROWSER',
-        link: 'https://adrikat2022.github.io/test_project/'
+        link: 'https://adrikat2022.github.io/Flutter-Space-Shooter/'
       },
       {
         type: 'GITHUB',
-        link: 'https://github.com/AdriKat2022/test_project'
+        link: 'https://github.com/AdriKat2022/Flutter-Space-Shooter'
       }
     ],
     title: {
